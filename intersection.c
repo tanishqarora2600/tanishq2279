38d17fb87025ec19d7ed532eb7e4ee80925d0dc7
tanishqarora2600 <56479357+tanishqarora2600@users.noreply.github.com>
2019-10-12 19:43:01 +0530

    Create intersection.c

---
+++ intersection.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[5],b[5],c[5];
+int i,j,same=0;
+printf("Enter set A \n");
+for(i=0;i<5;i++)
+    {
+     scanf("%d",&a[i]);
+    }
+printf("Enter set B \n");
+for(i=0;i<5;i++)
+    {
+     scanf("%d",&b[i]);
+    }
+printf("Intersection of set A and B is : ");
+for(i=0;i<5;i++)
+    {
+     same=0;
+     for(j=0;j<i;j++)
+        {
+         if(a[j]==a[i])
+            {
+              same=1;
+              break;
+            }
+        }
+    if(same==0)
+        {
+          for(j=0;j<5;j++)
+            {
+                 if(b[j]==a[i])
+                {
+                  printf("%d ",a[i]);
+                  break;
+                }
+            }
+        }
+    }
+ getch();
+}
