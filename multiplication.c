2e5e6c644c2e008103c8501b0d8be28db9c28e4a
tanishqarora2600 <56479357+tanishqarora2600@users.noreply.github.com>
2019-10-12 19:32:00 +0530

    Create multiplication.c

---
+++ multiplication.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int A[2][2],B[2][2],C[2][2],i,j,k;
+printf("Enter elements for Matrix A:\n ");
+for(i=0;i<2;i++)
+    {
+     for(j=0;j<2;j++)
+        {
+         scanf("%d",&A[i][j]);
+        }
+    }
+printf("Enter elements for Matrix B:\n ");
+for(i=0;i<2;i++)
+    {
+    for(j=0;j<2;j++)
+        {
+         scanf("%d",&B[i][j]);
+        }
+    }
+for(i=0;i<2;i++)
+    {
+    for(j=0;j<2;j++)
+        {
+            C[i][j]=0;
+            for(k=0;k<2;k++)
+            {
+                C[i][j] = C[i][j] + A[i][k]*B[k][j];
+
+            }
+        }
+    }
+printf("Multiplication of 2 matrices:\n ");
+for(i=0;i<2;i++)
+    {
+     for(j=0;j<2;j++)
+          {
+           printf("%d ",C[i][j]);
+          }
+     printf("\n");
+    }
+getch();
+}
