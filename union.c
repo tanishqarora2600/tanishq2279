a7ce865e69d091f52b893c29f97d531a7088c55c
meaditya007 <55221601+meaditya007@users.noreply.github.com>
2020-10-01 17:03:32 +0530

    Update union.c

--- union.c
+++ union.c
@@ -1,29 +1,29 @@
 #include<stdio.h>
 void main()
 {
-int a[6],b[6],c[12]:
+int a[5],b[5],c[10]:
 int i,j,same=0,y;
 printf("Enter Array A: \t");
-for(i=0;i<6;i++)
+for(i=0;i<5;i++)
     {
      scanf("%d",&a[i]);
     }
 printf("Enter Array B: \t");
     printf("%d",y);
-for(i=0;i<6;i++)
+for(i=0;i<5;i++)
     {
      scanf("%d", &b[i]);
     }
-for(i=0;i<6;i++)
+for(i=0;i<5;i++)
     {
      c[i]=a[i];
     }
-for(i=0,j=6;i<6,j<12;i++,j++)
+for(i=0,j=5;i<5,j<10;i++,j++)
     {
      c[j]=b[i];
     }
 printf("Union of Array A and B is :\t ");
-for(i=0;i<12;i++)
+for(i=0;i<10;i++)
     {
     same=0;
     for(j=0;j<i;j++)
