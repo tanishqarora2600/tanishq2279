e8524254c6a7590c66de35f286cb137fd5603245
bhavnasaxena96 <55221626+bhavnasaxena96@users.noreply.github.com>
2020-10-01 16:07:12 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -2,36 +2,36 @@
 #include<conio.h>
 void main()
 {
-int a[3][3],b[3][3],c[3][3]=0,i=0,j=0;
+int a[4][4],b[4][4],c[4][4]=0,i=0,j=0;
 printf("Enter elements for Matrix A: \n");
-for(i=0;i<3;i++)
+for(i=0;i<4;i++)
     
     
     {
-     for(j=0;j<3;j++)
+     for(j=0;j<4;j++)
         {
          scanf("%d ",&a[i][j]);
         }
      }
 printf("Enter elements for Matrix B:\n");
-for(i=0;i<3;i++)
+for(i=0;i<4;i++)
       {
-       for(j=0;j<3;j++)
+       for(j=0;j<4;j++)
         {
          scanf("%d",&b[i][j]);
         }
     }
 printf("Addition of 2 Matrices = \n");
-for(i=0;i<3;i++)
+for(i=0;i<4;i++)
     {
-     for(j=0;j<3;j++)
+     for(j=0;j<4;j++)
         {
             c[i][j] = b[i][j] + a[i][j];
         }
     }
-for(i=0;i<3;i++)
+for(i=0;i<4;i++)
     {
-     for(j=0;j<;j++)
+     for(j=0;j<4;j++)
             {
              printf("%d   ",c[i][j]);
             }
